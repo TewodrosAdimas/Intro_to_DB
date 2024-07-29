e2de822250935bf67e378c79d0462e7bdb3c65d8
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-29 11:50:16 +0300

    db

---
+++ task_2.sql
@@ -0,0 +1,42 @@
+import mysql.connector
+connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Tedi@1234'
+        )
+
+USE alx_book_store;
+
+
+CREATE TABLE Books(
+    book_id (PRIMARY KEY)
+    title VARCHAR(130)
+    author_id (FOREIGN KEY)
+    price DOUBLE
+    publication_date DATE
+)
+
+CREATE TABLE Authors(
+    author_id (PRIMARY KEY)
+    author_name VARCHAR(215)
+)
+
+CREATE TABLE Customers(
+    customer_id (PRIMARY KEY)
+    customer_name VARCHAR(215)
+    email VARCHAR(215)
+    address TEXT
+)
+
+CREATE TABLE Orders(
+    order_id INT (PRIMARY KEY)
+    customer_id INT (FOREIGN KEY (customer_id) REFERENCES Customers(customer_id))
+    order_date DATE
+)
+
+CREATE TABLE Order_Details(
+    orderdetailid INT (PRIMARY KEY)
+    order_id INT (FOREIGN KEY (order_id) REFERENCES Orders(order_id))
+    book_id INT (FOREIGN KEY (book_id) REFERENCES Books(book_id))
+    quantity DOUBLE
+)
