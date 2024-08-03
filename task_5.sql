824f2412c9dddef00749401a9899008ee5bcad79
TewodrosAdimas <yosefenadagim@gmail.com>
2024-08-03 21:10:00 +0300

    db

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+SET @DBNAME = 'alx_book_store';
+
+INSERT INTO @DBNAME.customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
